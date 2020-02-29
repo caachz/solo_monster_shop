10d6779a90ed918dd578a88c29cbdc5706e87a49
Cassie Achzenick <caachzenick@gmail.com>
2020-02-29 09:50:00 -0700

    Add descriptions for which tests we will need

---
+++ spec/features/employees/discount_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "As an merchant employee,", type: :feature do
+  describe "when I visit my merchants dashboard" do
+    it "A link takes me to my bulk discounts page" do
+    end
+
+    it "On my bulk discounts page I can create a new bulk discount" do
+    end
+
+    it "On my bulk discounts page I can edit an existing bulk discount" do
+    end
+
+    it "On my bulk discounts page I can delete an existing bulk discount" do
+    end
+
+    it "I can have many bulk discounts and only the highest discount will apply" do
+    end
+  end
+end
