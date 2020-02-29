b6a15d354d5ea0b72c1266e3002daf811dbd334d
Cassie Achzenick <caachzenick@gmail.com>
2020-02-29 14:56:22 -0700

    Add feature test for new discount

--- spec/features/employees/discount_spec.rb
+++ spec/features/employees/discount_spec.rb
@@ -29,6 +29,36 @@ RSpec.describe "As an merchant employee,", type: :feature do
     end
 
     it "On my bulk discounts page I can create a new bulk discount" do
+      user = create(:user, role: 1)
+
+      mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      mike = Merchant.create!(name: "Monsters Inc Shoppe", address: '334 nothing st', city: 'Moab', state: 'UT', zip: 48903)
+
+      mike.users << user
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      discount1 = Discount.create!(merchant: mike, quantity: 20, discount: 20)
+      discount1 = Discount.create!(merchant: mike, quantity: 10, discount: 10)
+      discount1 = Discount.create!(merchant: mike, quantity: 5, discount: 5)
+
+      visit "/merchant/discounts/#{mike.id}"
+
+      click_link "Add a new discount!"
+
+      expect(current_path).to eq("/merchant/discounts/#{mike.id}/new")
+
+      fill_in "Quantity Threshold", with: "7"
+      fill_in "Discount", with: "75"
+
+      click_on "Submit"
+
+      expect(current_path).to eq("/merchant/discounts/#{mike.id}")
+
+      expect(page).to have_content('Discount 20% on 20 items or more')
+      expect(page).to have_content('Discount 10% on 10 items or more')
+      expect(page).to have_content('Discount 5% on 5 items or more')
+      expect(page).to have_content('Discount 75% on 7 items or more')
     end
 
     it "On my bulk discounts page I can edit an existing bulk discount" do
