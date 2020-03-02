3b50aa107f262f3da65141fd98a15741ff629842
Cassie Achzenick <caachzenick@gmail.com>
2020-03-01 17:21:38 -0700

    Add feature test that discount is applied in cart

--- spec/features/cart/discount_spec.rb
+++ spec/features/cart/discount_spec.rb
@@ -3,6 +3,65 @@ require 'rails_helper'
 RSpec.describe 'Bulk discounts applied in cart' do
   describe 'When I have enough of an item in my cart' do
     it "I see a discount applied to only that item" do
+      merchant1 = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      merchant2 = Merchant.create!(name: "Monsters Inc Shoppe", address: '334 nothing st', city: 'Moab', state: 'UT', zip: 48903)
+
+      discount1 = Discount.create!(merchant: merchant1, quantity: 10, discount: 10)
+
+      item1 = merchant1.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+      item2 = merchant1.items.create(name: "stuffed octopus", description: "snuggle bunnt", price: 64, image: "https://images-na.ssl-images-amazon.com/images/I/61yTBRbtvrL._AC_SY355_.jpg", inventory: 12)
+      item3 = merchant2.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+      visit "/items/#{item1.id}"
+      click_on "Add To Cart"
+
+      visit "/items/#{item2.id}"
+      click_on "Add To Cart"
+
+      visit "/items/#{item3.id}"
+      click_on "Add To Cart"
+
+      visit "/cart"
+
+      within "#cart-item--#{item2.id}" do
+        expect(page).to have_content('1')
+        expect(page).to have_content("$64.00")
+        click_on "Add Quantity"
+        expect(page).to have_content('2')
+        expect(page).to have_content("$128.00")
+        click_on "Add Quantity"
+        expect(page).to have_content('3')
+        expect(page).to have_content("$172.80")
+        expect(page).to have_content("10% discount added")
+      end
+
+      within "#cart-item--#{item1.id}" do
+        expect(page).to have_content('1')
+        expect(page).to have_content("$20.00")
+        click_on "Add Quantity"
+        expect(page).to have_content('2')
+        expect(page).to have_content("$40.00")
+        click_on "Add Quantity"
+        expect(page).to have_content('3')
+        expect(page).to have_content("$54.00")
+        expect(page).to have_content("10% discount added")
+      end
+
+      within "#cart-item--#{item3.id}" do
+        expect(page).to have_content('1')
+        expect(page).to have_content('$2.00')
+        click_on "Add Quantity"
+        expect(page).to have_content('2')
+        expect(page).to have_content('$4.00')
+        click_on "Add Quantity"
+        expect(page).to have_content('3')
+        expect(page).to have_content('$6.00')
+        expect(page).to_not have_content("10% discount added")
+      end
+    end
+
+    it "I can have many bulk discounts and only the highest discount will apply" do
+
     end
 
     it "I can have multiple different discounts for different quantities of items" do
--- spec/features/employees/discount_spec.rb
+++ spec/features/employees/discount_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe "As an merchant employee,", type: :feature do
       user = create(:user, role: 1)
 
       mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      mike = Merchant.create!(name: "Monsters Inc Shoppe", address: '334 nothing st', city: 'Moab', state: 'UT', zip: 48903)
+      merchant2 = Merchant.create!(name: "Monsters Inc Shoppe", address: '334 nothing st', city: 'Moab', state: 'UT', zip: 48903)
 
       mike.users << user
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       discount1 = Discount.create!(merchant: mike, quantity: 20, discount: 20)
-      discount1 = Discount.create!(merchant: mike, quantity: 10, discount: 10)
+      discount1 = Discount.create!(merchant: mike, quantity: 10, discount: 3)
       discount1 = Discount.create!(merchant: mike, quantity: 5, discount: 5)
 
       visit '/merchant'
@@ -121,9 +121,6 @@ RSpec.describe "As an merchant employee,", type: :feature do
       expect(page).to_not have_content('Discount 20% on 20 items or more')
       expect(page).to have_content('Discount 10% on 10 items or more')
       expect(page).to have_content('Discount 5% on 5 items or more')
-    end
-
-    it "I can have many bulk discounts and only the highest discount will apply" do
     end
   end
 end
